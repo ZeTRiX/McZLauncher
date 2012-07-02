c75f1428c562bde145555658690222a04cf2f321
Evgen <zetrix.95@gmail.com>
2012-07-02 13:06:19 +0400

    License added
    Now this program distributes under GNU General Public License v3.0

--- ru/zetrix/settings/ShieldUtil.java
+++ ru/zetrix/settings/ShieldUtil.java
@@ -1,3 +1,21 @@
+/*
+ * McZLauncher (ZeTRiX's Minecraft Launcher)
+ * Copyright (C) 2012 Evgen Yanov <http://www.zetlog.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program (In the "_License" folder). If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package ru.zetrix.settings;
 
 import java.io.UnsupportedEncodingException;
@@ -8,31 +26,7 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Formatter;
 import java.util.Locale;
 
-/**
- *
- * @author ZeTRiX
- */
 public class ShieldUtil {
-    public static String MACAddr() {
-        try {
-            NetworkInterface ni = NetworkInterface.getByInetAddress(InetAddress.getLocalHost());
-            if (ni != null) {
-                StringBuilder sb = new StringBuilder();
-                Formatter formatter = new Formatter(sb, Locale.US);
-                byte[] mac = ni.getHardwareAddress();
-                if (mac != null)
-                    for (int i = 0; i < mac.length; i++) {
-                        formatter.format("%02X%s", new Object[] { Byte.valueOf(mac[i]), i < mac.length - 1 ? "-" : "" });
-                        if (formatter.toString().equals("")) return "NO_MAC_Address";
-                    }
-                return formatter.toString();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "NO_MAC_Address";
-    }
-    
     public static String ShaHash(String pass) {
         String sha1 = "";
         try {
@@ -46,6 +40,26 @@ public class ShieldUtil {
             e.printStackTrace();
         }
         return sha1;
+    }
+    
+    public static String MACAddr() {
+        try {
+            NetworkInterface ni = NetworkInterface.getByInetAddress(InetAddress.getLocalHost());
+            if (ni != null) {
+                StringBuilder sb = new StringBuilder();
+                Formatter formatter = new Formatter(sb, Locale.US);
+                byte[] mac = ni.getHardwareAddress();
+                if (mac != null)
+                    for (int i = 0; i < mac.length; i++) {
+                        formatter.format("%02X%s", new Object[] { Byte.valueOf(mac[i]), i < mac.length - 1 ? "-" : "" });
+                        if (formatter.toString().equals("")) return "Fail";
+                    }
+                return formatter.toString();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "Fail";
     }
     
     private static String byteToHex(final byte[] hash) {
--- ru/zetrix/settings/Util.java
+++ ru/zetrix/settings/Util.java
@@ -1,20 +1,28 @@
+/*
+ * McZLauncher (ZeTRiX's Minecraft Launcher)
+ * Copyright (C) 2012 Evgen Yanov <http://www.zetlog.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program (In the "_License" folder). If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package ru.zetrix.settings;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
 import javax.imageio.ImageIO;
 
-/**
- *
- * @author ZeTRiX
- */
-public class Util {	
-	/**
-	 * Функция нахождения рабочей директории клиента.
-	 * В качестве параметра - рабочая папка клиента.
-	 * @param workDir
-	 * @return
-	 */
+public class Util {
     public static File workDir = null;
     private static ConfigUtils ConfigFile = new ConfigUtils("/ru/zetrix/res/MZL.config", new File(getWorkingDirectory(), "MZL.config"));
     static {
